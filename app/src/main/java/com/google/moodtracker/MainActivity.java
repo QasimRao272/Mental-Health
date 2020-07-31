2d37995b209e2c2254a5a0f695574faeba2aadc6
Qasim Rao <qasimrao272@gmail.com>
2020-07-30 23:43:41 -0700

    Demo Ended !

--- app/src/main/java/com/google/moodtracker/MainActivity.java
+++ app/src/main/java/com/google/moodtracker/MainActivity.java
@@ -38,10 +38,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }*/
+        }
 
         /* img_plus = findViewById(R.id.img_plus);
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
